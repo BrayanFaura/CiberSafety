77143580717b122ee8db36b4140175b2e7e64e9b
BrayanFaura <est.brayan.faura@unimilitar.edu.co>
2022-05-21 17:29:17 -0500

    incorporando el audio de Ben en las secciones terminadas

--- js/scripts.js
+++ js/scripts.js
@@ -175,15 +175,282 @@ function showHide33(){
     oImageDiv.style.display =(oImageDiv.style.display=='none')?'inline':'none'
 }
 
-function pausaTodo(){
-	var inputs = document.getElementsByTagName('img');
-  for(var i = 0; i < inputs.length; i++) {
-      inputs[i].pause();
-  }
+// SONIDOS BEN INTRO
+
+function reproducirsonidobi1(){
+    let imagen = document.querySelector(".reproductorbi1")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benintro1.mp3");
+    etiquetaAudio.play();
+    })
 }
 
+function reproducirsonidobi2(){
+    let imagen = document.querySelector(".reproductorbi2")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benintro2.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+function reproducirsonidobi3(){
+    let imagen = document.querySelector(".reproductorbi3")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benintro3.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+function reproducirsonidobi4(){
+    let imagen = document.querySelector(".reproductorbi4")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benintro4.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+function reproducirsonidobi5(){
+    let imagen = document.querySelector(".reproductorbi5")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benintro5.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+function reproducirsonidobi6(){
+    let imagen = document.querySelector(".reproductorbi6")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benintro6.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+function reproducirsonidobi7(){
+    let imagen = document.querySelector(".reproductorbi7")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benintro7.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+function reproducirsonidobi8(){
+    let imagen = document.querySelector(".reproductorbi8")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benintro8.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+function reproducirsonidobi9(){
+    let imagen = document.querySelector(".reproductorbi9")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benintro9.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+function reproducirsonidobi10(){
+    let imagen = document.querySelector(".reproductorbi10")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benintro10.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+//SONIDO MATT Y BEN EN CONJUNTO PARA LA INTRO
+
+function reproducirsonidobymi1(){
+    let imagen = document.querySelector(".reproductorbymi1")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/BENYMATT/iajuntasintro1.mp3");
+    etiquetaAudio.play();
+    })
+}
+function reproducirsonidobymi2(){
+    let imagen = document.querySelector(".reproductorbymi2")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/BENYMATT/iajuntasintro1.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+//SONIDOS BEN PHISHING
+
+function reproducirsonidobp1(){
+    let imagen = document.querySelector(".reproductorbp1")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing1.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp2(){
+    let imagen = document.querySelector(".reproductorbp2")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing2.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp3(){
+    let imagen = document.querySelector(".reproductorbp3")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing3.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp4(){
+    let imagen = document.querySelector(".reproductorbp4")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing4.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp5(){
+    let imagen = document.querySelector(".reproductorbp5")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing5.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp6(){
+    let imagen = document.querySelector(".reproductorbp6")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing6.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp7(){
+    let imagen = document.querySelector(".reproductorbp7")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing7.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp8(){
+    let imagen = document.querySelector(".reproductorbp8")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing8.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp9(){
+    let imagen = document.querySelector(".reproductorbp9")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing9.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp10(){
+    let imagen = document.querySelector(".reproductorbp10")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing10.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp11(){
+    let imagen = document.querySelector(".reproductorbp11")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing11.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp12(){
+    let imagen = document.querySelector(".reproductorbp12")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing12.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp13(){
+    let imagen = document.querySelector(".reproductorbp13")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing13.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp14(){
+    let imagen = document.querySelector(".reproductorbp14")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing14.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp15(){
+    let imagen = document.querySelector(".reproductorbp15")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing15.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+
+function reproducirsonidobp16(){
+    let imagen = document.querySelector(".reproductorbp16")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benphishing16.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+// SONIDOS BEN EVALUACION PHISHING
+
 function reproducirsonidobep1(){
-    pausaTodo();
+    // pausaTodo();
     let imagen = document.querySelector(".reproductorbep1")
     imagen.addEventListener("click", () => {
     let etiquetaAudio = document.createElement("audio");
@@ -197,6 +464,24 @@ function reproducirsonidobep2(){
     imagen.addEventListener("click", () => {
     let etiquetaAudio = document.createElement("audio");
     etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benevaluacionp2.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+function reproducirsonidobep3(){
+    let imagen = document.querySelector(".reproductorbep3")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benevaluacionp3.mp3");
+    etiquetaAudio.play();
+    })
+}
+
+function reproducirsonidobep4(){
+    let imagen = document.querySelector(".reproductorbep4")
+    imagen.addEventListener("click", () => {
+    let etiquetaAudio = document.createElement("audio");
+    etiquetaAudio.setAttribute("src", "assets/sounds/IABEN/Benevaluacionp4.mp3");
     etiquetaAudio.play();
     })
 }
